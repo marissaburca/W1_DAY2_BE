9ebdbda398027d9530c836becd5640bedee4588e
Marissa Burca <Marissaburca@gmail.com>
2023-11-28 18:28:11 +0100

    done ex 3

--- src/exercise3/main3.java
+++ src/exercise3/main3.java
@@ -3,15 +3,17 @@ package exercise3;
 import java.util.Scanner;
 
 public class main3 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Type a word, I'll return it in characters");
-        String word = userInput.nextLine();
-        char[] arrayWord = word.toCharArray();
-        int i = 0;
-        while (i < arrayWord.length && arrayWord[i] != ('q')) {
-            System.out.println(arrayWord[i]+",");
-            i++;
-        }
+        String word= "";
+        do {
+            System.out.println("Type a word, I'll return it in characters");
+            word = userInput.nextLine();
+            String[] arrayWord = word.split("");
+            System.out.println(String.join(",", arrayWord));
+        } while (!word.equals(":q"));
+            userInput.close();
+            System.out.println("Exit");
+
     }
 }
